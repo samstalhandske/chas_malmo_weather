ee476db96a333bf322833fbb6f81fbf8be2b6643
Jonathan Akestam <jonathan.alestam@gmail.com>
2025-09-15 14:43:32 +0200

    Added main loop and welcome message

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-	printf("Hello, world!\n");
+	printf("Welcome to this weather app\n");
+
+	bool programShouldExit = false;
+
+	while (programShouldExit == false) {
+		
+	}
+
 	return 0;
 }
+ 
