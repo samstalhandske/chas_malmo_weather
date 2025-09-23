730a538ab5a37a712f769f8c8ff0ff6cd7c4d112
Emilio <xxglobalelitexx123@gmail.com>
2025-09-23 17:49:49 +0200

    working

--- src/WeatherReport/Get_Weather_Report.c
+++ src/WeatherReport/Get_Weather_Report.c
@@ -27,7 +27,7 @@ char* WMOInterpreter(int _WMOCode);
     char* description;      verbose weather description from OpenMeteo docs: https://open-meteo.com/en/docs#weather_variable_documentation
 */
 
-Weather_Report* Get_Weather_Report(char* _CityName, double _Latitude, double _Longitude){
+Weather_Report Get_Weather_Report(char* _CityName, double _Latitude, double _Longitude){
     char api_url[256]; /* "https://api.open-meteo.com/v1/forecast?latitude=%lf&longitude=%lf&current_weather=true"; */
     char* JsonString = malloc(strlen(_CityName) + 6); /* +4 for ".json" and +1 for null terminator */
     sprintf(JsonString, "%s.json", _CityName);
@@ -37,18 +37,14 @@ Weather_Report* Get_Weather_Report(char* _CityName, double _Latitude, double _Lo
     printf("api_url: %s\n", api_url);
 
 
-    Weather_Report* New_Weather_Report = malloc(sizeof(Weather_Report));
-    if (New_Weather_Report == NULL) {
-        fprintf(stderr, "Failed to allocate memory for Weather_Report.\n");
-        return NULL;
-    }
+    Weather_Report New_Weather_Report = {0};
     /* else printf("Get_Weather_Report>> New_Weather_Report memory alloc success\n"); */
 
 
 
     /* HTTP CALL */
     Http myHttp;
-    Http_Response* Response = calloc(1, sizeof(Http_Response));
+    Http_Response response = {0};
 
     int ErrCode = Http_Initialize(&myHttp);
     assert(ErrCode == 0);
@@ -57,24 +53,23 @@ Weather_Report* Get_Weather_Report(char* _CityName, double _Latitude, double _Lo
 
     
 
-    int perErrCode = Http_Perform(&myHttp, api_url, Response);
+    int perErrCode = Http_Perform(&myHttp, api_url, &response);
     if(perErrCode != 0){
         printf("Get_Weather_Report>> http-Perform, error-code: %i.\n", perErrCode);
-        printf("Response->data content ( %i bytes): %s\n", Response->size, Response->data);
+        printf("Response->data content ( %i bytes): %s\n", response.size, response.data);
     }
 
     assert(perErrCode == 0);
     
-    if (Response == NULL || Response->data == NULL || Response->size == 0) {
+    if (response.data == NULL || response.size == 0) {
         fprintf(stderr, "Http_Perform returned success but response is empty.\n");
-        Http_Dispose_Response(Response);
+        Http_Dispose_Response(&response);
         /*free(Response);*/
-        free(New_Weather_Report);
         return NULL;
     }
     
     
-    cJSON* JsonRoot = cJSON_Parse(Response->data);
+    cJSON* JsonRoot = cJSON_Parse(response.data);
 
     if(DoesFileExist(JsonString)) {
         struct stat filinfo;
@@ -101,6 +96,7 @@ Weather_Report* Get_Weather_Report(char* _CityName, double _Latitude, double _Lo
         
         
     }
+    free(JsonString);
     
     
 
@@ -137,14 +133,14 @@ Weather_Report* Get_Weather_Report(char* _CityName, double _Latitude, double _Lo
     int WCode = atof(cJSON_Print(weathercode));
     char* WeatherVerbose = WMOInterpreter(WCode);
 
-    New_Weather_Report->cityname = _CityName;
-    New_Weather_Report->latitude = _Latitude;
-    New_Weather_Report->longitude = _Longitude;
-    New_Weather_Report->timestamp = TimeLong;
-    New_Weather_Report->windspeed = WindSpeed;
-    New_Weather_Report->temperature = Temperature;
-    New_Weather_Report->weathercode = WCode;
-    New_Weather_Report->description = WeatherVerbose;
+    New_Weather_Report.cityname = _CityName;
+    New_Weather_Report.latitude = _Latitude;
+    New_Weather_Report.longitude = _Longitude;
+    New_Weather_Report.timestamp = TimeLong;
+    New_Weather_Report.windspeed = WindSpeed;
+    New_Weather_Report.temperature = Temperature;
+    New_Weather_Report.weathercode = WCode;
+    New_Weather_Report.description = WeatherVerbose;
 
     /*
     printf("\n\tCity:\t\t%s\n", _CityName);
@@ -156,10 +152,9 @@ Weather_Report* Get_Weather_Report(char* _CityName, double _Latitude, double _Lo
 */
 
     cJSON_Delete(JsonRoot);
-    Http_Dispose_Response(Response);
+    Http_Dispose_Response(&response);
     Http_Dispose(&myHttp);
     /*free(Response); */
-    Response = NULL;
     return New_Weather_Report;
 }
 
