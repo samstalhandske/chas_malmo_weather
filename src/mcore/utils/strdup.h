427c42ade9f580cd98f2b0c6fd288157c4b77221
lundhpargmailcom <lundh.par@gmail.com>
2025-09-29 13:58:17 +0200

    Added header guards

--- src/mcore/utils/strdup.h
+++ src/mcore/utils/strdup.h
@@ -1,2 +1,6 @@
+#ifndef STRDUP_H
+#define STRDUP_H
 
 char* strdup(const char* str);
+
+#endif
--- src/ui/UserInteractions.h
+++ src/ui/UserInteractions.h
@@ -1,5 +1,12 @@
+#ifndef USERINTERACTIONS_H
+#define USERINTERACTIONS_H
+
+
 #include "../city/City.h"
 
 int UserSelectCity(LinkedListCities* _LLC, City** _SelectedCity);
 
 char* UserSelectCityChar();
+
+
+#endif
